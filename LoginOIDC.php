f9b70884a61b9548eb9432be9a6cc6fdf73531de
Dominik Thiemermann <dominik@dthiemermann.org>
2019-03-30 18:17:50 +0100

    Put comments in code and follow psr styleguidlines

--- LoginOIDC.php
+++ LoginOIDC.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Piwik - free/libre analytics platform
  *
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
+
 namespace Piwik\Plugins\LoginOIDC;
 
 use Exception;
@@ -14,64 +16,97 @@ use Piwik\FrontController;
 
 class LoginOIDC extends \Piwik\Plugin
 {
+    /**
+     * Subscribe to Matomo events and assign handlers.
+     * https://developer.matomo.org/api-reference/Piwik/Plugin#registerevents
+     *
+     * @return array
+     */
+    public function registerEvents()
+    {
+        return array(
+            "AssetManager.getStylesheetFiles" => "getStylesheetFiles",
+            "Template.userSettings.afterTokenAuth" => "renderLoginOIDCUserSettings",
+            "Template.loginNav" => "renderLoginOIDCMod"
+        );
+    }
 
-  public function registerEvents()
-  {
-    return array(
-      'AssetManager.getStylesheetFiles' => 'getStylesheetFiles',
-      'Template.userSettings.afterTokenAuth' => 'renderLoginOIDCUserSettings',
-      'Template.loginNav' => 'renderLoginOIDCMod'
-    );
-  }
+    /**
+     * Append additional stylesheets.
+     *
+     * @param  array  $files
+     * @return void
+     */
+    public function getStylesheetFiles(&$files)
+    {
+        $files[] = "plugins/LoginOIDC/stylesheets/loginMod.css";
+    }
 
-  public function getStylesheetFiles(&$files)
-  {
-    $files[] = "plugins/LoginOIDC/stylesheets/loginMod.css";
-  }
+    /**
+     * Append custom user settings layout.
+     *
+     * @param  string  $out
+     * @return void
+     */
+    public function renderLoginOIDCUserSettings(&$out)
+    {
+        $content = FrontController::getInstance()->dispatch("LoginOIDC", "userSettings");
+        if (!empty($content)) {
+            $out .= $content;
+        }
+    }
 
-  public function install()
-  {
-    try {
-      // right now there is just one provider but we already add a column to support multiple providers later on
-      $sql = "CREATE TABLE " . Common::prefixTable('loginoidc_provider') . " (
+    /**
+     * Append login oauth button layout.
+     *
+     * @param  string       $out
+     * @param  string|null  $payload
+     * @return void
+     */
+    public function renderLoginOIDCMod(&$out, $payload)
+    {
+        if ($payload === "bottom") {
+            $content = FrontController::getInstance()->dispatch("LoginOIDC", "loginMod");
+            if (!empty($content)) {
+                $out .= $content;
+            }
+        }
+    }
+
+    /**
+     * Extend database.
+     *
+     * @return void
+     */
+    public function install()
+    {
+        try {
+            // right now there is just one provider but we already add a column to support multiple providers later on
+            $sql = "CREATE TABLE " . Common::prefixTable("loginoidc_provider") . " (
                 user VARCHAR( 100 ) NOT NULL,
                 provider_user VARCHAR( 255 ) NOT NULL,
                 provider VARCHAR( 255 ) NOT NULL,
                 date_connected TIMESTAMP NOT NULL,
                 PRIMARY KEY ( provider_user, provider ),
-                FOREIGN KEY ( user ) REFERENCES " . Common::prefixTable('user') . "( login ) ON DELETE CASCADE,
+                FOREIGN KEY ( user ) REFERENCES " . Common::prefixTable("user") . "( login ) ON DELETE CASCADE,
                 CONSTRAINT user_provider UNIQUE ( user, provider )
-              ) DEFAULT CHARSET=utf8";
-      Db::exec($sql);
-    } catch(Exception $e) {
-      // ignore error if table already exists (1050 code is for 'table already exists')
-      if (!Db::get()->isErrNo($e, '1050')) {
-        throw $e;
-      }
+                ) DEFAULT CHARSET=utf8";
+            Db::exec($sql);
+        } catch(Exception $e) {
+            // ignore error if table already exists (1050 code is for 'table already exists')
+            if (!Db::get()->isErrNo($e, "1050")) {
+                throw $e;
+            }
+        }
     }
-  }
 
-  public function uninstall()
-  {
-    Db::dropTables(Common::prefixTable('loginoidc_provider'));
-  }
-
-  public function renderLoginOIDCUserSettings(&$out)
-  {
-    $content = FrontController::getInstance()->dispatch('LoginOIDC', 'userSettings');
-    if (!empty($content)) {
-      $out .= $content;
+    /**
+     * Undo database changes from install.
+     *
+     * @return void
+     */
+    public function uninstall()
+    {
+        Db::dropTables(Common::prefixTable("loginoidc_provider"));
     }
-  }
-
-  public function renderLoginOIDCMod(&$out, $payload)
-  {
-    if ($payload === 'bottom') {
-      $content = FrontController::getInstance()->dispatch('LoginOIDC', 'loginMod');
-      if (!empty($content)) {
-        $out .= $content;
-      }
-    }
-  }
-
 }
